da48c6ea68a562281f0a0d5e0cdfde09c5946e52
pedrojunior <ph751153@gmail.com>
2024-02-11 20:42:17 -0300

    Funções extras

--- ProtótipoJogo.py
+++ ProtótipoJogo.py
@@ -8,9 +8,51 @@ def limpa():
     os.system('cls' if os.name == "nt"  else "clear")
 
 
-def linha(tipo="=+=",tam=30):
-    print("\033[1;32m")
+def linha(tipo="=+=",tam=30,cor="\033[1;32m"):
+    print(cor)
     print(tipo*tam)
+
+
+def mensagemErro(msg):
+    limpa()
+    linha("+-+",20,cor="\033[1;31m")
+    print("""\033[1;31m
+          ERROR
+        .e$$$$e.
+       e$$$$$$$$$$e
+      $$$$$$$$$$$$$$
+     d$$$$$$$$$$$$$$b
+     $$$$$$$$$$$$$$$$
+    4$$$$$$$$$$$$$$$$F
+    4$$$$$$$$$$$$$$$$F
+     $$$" "$$$$" "$$$
+     $$F   4$$F   4$$
+     '$F   4$$F   4$"
+      $$   $$$$   $P
+      4$$$$$"^$$$$$%
+       $$$$F  4$$$$
+        "$$$ee$$$"
+        . *$$$$F4
+         $     .$
+         "$$$$$$"
+          ^$$$$      
+    """)
+    linha("+-+",20,"\033[1;31m")
+    tempo(2)
+    limpa()
+    linha("===",20,"\033[1;31m")
+    print(f"{msg.center(60)}")
+    linha("===",20,"\033[1;31m")
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            a = int(input(msg))
+        except:
+            mensagemErro("DIGITE NOVAMENTE!")
+        else:
+            return a
 
 
 def telaMenu(*opcs):
@@ -24,7 +66,7 @@ def telaMenu(*opcs):
     /                                               
     -------------------------------------------------
     """)
-    
+    opc = leiaInt('Qual sua opção? ')
 
 
 def layoutQuadrado(msg):
@@ -36,7 +78,7 @@ def layoutQuadrado(msg):
         print(f"{letras[0]}")
         print("\033[m")
         linha("---",21)
-        tempo(0.5)
+        tempo(0.3)
         limpa()
     telaMenu("JOGAR", "FINALIZAR JOGO", "SOBRE")
 
